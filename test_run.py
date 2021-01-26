482e8264b831a29a786f8dcc9a7591a0c587eede
Szymon Mikler <sjmikler@gmail.com>
2021-01-26 22:26:58 +0100

    Added time output

--- test_run.py
+++ test_run.py
@@ -10,8 +10,6 @@ if __name__ == "__main__":
     with open(flags.RACE_CONFIG, 'r') as f:
         race_config = f.read()
 
-    t = time.time()
-
     # Create artificial configs
     xml_config_paths = []
     for idx in range(NUM_RACES):
@@ -23,7 +21,11 @@ if __name__ == "__main__":
         xml_config_paths.append(new_race_config_path)
 
     # Run configured races
+    t = time.time()
     results = tools.run_races_read_results(xml_config_paths)
+    tt = time.time() - t
+
     for i, r in enumerate(results):
         print(f'{i}: {r}')
-    print("Time taken:", time.time() - t)
+
+    print(f"Time taken: {tt:.2f}s ({tt/NUM_RACES:.2f}s per race)")
