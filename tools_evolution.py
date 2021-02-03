39623c8c7d32db86345026e8e56a0e393f6607c3
Michał Jaworski <jawor403@gmail.com>
2021-02-03 21:43:49 +0100

    Ignore first lap in speed_entropy

--- tools_evolution.py
+++ tools_evolution.py
@@ -1,24 +1,24 @@
 import numpy as np
 
 
-def bin_entropy(a, bins=16):
+def bin_entropy(a, bins=16, range=None):
     """Calculate entropy from bins, the same is done in the paper
 
     :param a: Input data
     :param bins: int or sequence, passed to np.histogram
+    :param range: range of possible values, passed to np.histogram
     """
 
-    p, _ = np.histogram(a, bins, density=True)
-    p1 = p[p > 0.0]
+    p, _ = np.histogram(a, bins=bins, range=range, density=True)
+    p1 = p[p > 0.]
     return -np.sum(p1 * np.log2(p1))
 
 
 def speed_entropy(race_data):
-    # ignore the time before the race starts (perhaps it's better to remove the first lap altogether)
-    race_data = race_data[race_data.time >= 0.0]
+    # ignore data from the first lap and laps after the last one
+    # (they keep counting until the last player finishes)
+    race_data = race_data[race_data.lap.isin([2, 3, 4])]
     # speed of all players, for all ticks
     speed = np.sqrt(race_data.vx ** 2 + race_data.vy ** 2 + race_data.vz ** 2)
-    # equal size bins for now
-    if any(np.isnan(speed)):
-        return None
-    return bin_entropy(speed, bins=16)
+    # equal size bins for now, ignore very low and very high speeds
+    return bin_entropy(speed, bins=16, range=(10, 90))
